3fd7309b3be4c852611edd5aaac536b40b32a9f2
Anthony <zeitlin1@gmail.com>
2018-03-24 16:22:15 -0400

    Initial Commit

---
+++ CallyoObjcCodeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallyoObjcCodeChallenge
+//
+//  Created by Anthony on 3/24/18.
+//  Copyright © 2018 Anthony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CallyoObjcCodeChallenge/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CallyoObjcCodeChallenge
+//
+//  Created by Anthony on 3/24/18.
+//  Copyright © 2018 Anthony. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
